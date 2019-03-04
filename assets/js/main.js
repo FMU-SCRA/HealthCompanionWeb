d1571c19aa3f2af1e7c144c67d8b38fdb9ffe766
FMU-SCRA <41973262+FMU-SCRA@users.noreply.github.com>
2019-03-04 14:34:39 -0500

    Added Main Files
    added the main files but will rename and edit soon.

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+$(document).ready(function() {
+  // slider
+  var slider = $('#slider').slideReveal(); // slideReveal return $('#slider')
+
+  $("#sidebar-wrapper").slideReveal({
+    trigger: $("#toggle"),
+    push: false,
+    speed: 200,
+    overlay: true
+  });
+
+  // this allows the second button to close the menu
+  $("#toggleSidebar").mousedown(function() {
+    $('#sidebar-wrapper').slideReveal("toggle");
+  });
+
+  
+
+  /********
+  ABOUT PAGE
+  *********/
+
+  // document.write(mattAboutParagraph);
+
+
+});
