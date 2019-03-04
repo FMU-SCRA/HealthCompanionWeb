8262218df7166e05fcb0fd122fd913a79583eed0
Matt Harrington <mharr1729@gmail.com>
2019-03-04 15:47:34 -0500

    got javascript working with form

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,13 +16,32 @@ $(document).ready(function() {
     $('#sidebar-wrapper').slideReveal("toggle");
   });
 
-  
-
-  /********
-  ABOUT PAGE
-  *********/
-
-  // document.write(mattAboutParagraph);
-
-
 });
+
+document.getElementById('clinicForm').addEventListener('submit', submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+  console.log(123);
+
+var clinicName = getInputVal('clinicName');
+var address = getInputVal('address');
+var city = getInputVal('city');
+var state = getInputVal('state');
+var phone = getInputVal('phone');
+var zip = getInputVal('zip');
+var hours = getInputVal('hours');
+var website = getInputVal('url');
+var latitude = getInputVal('lat');
+var longitude = getInputVal('longitude');
+
+console.log(clinicName);
+
+// below will be the services in message
+var services = getInputVal('services');
+
+}
+
+function getInputVal(id) {
+  return document.getElementById(id).value;
+}
