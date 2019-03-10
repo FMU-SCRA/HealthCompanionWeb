62d25a729215e20c68a9789cee39652586ec937a
Matt Harrington <mharr1729@gmail.com>
2019-03-10 01:11:45 -0500

    working on features page

---
+++ assets/js/features.js
@@ -0,0 +1,25 @@
+// const gra = function(min, max) {
+//     return Math.random() * (max - min) + min;
+// };
+// const init = function() {
+//     let items = document.querySelectorAll("li");
+//     for (let i = 0; i < items.length; i++) {
+//         items[i].style.minHeight = gra(120, 205) + "vh";
+//         items[i].style.background = randomColor({ luminosity: "light" });
+//     }
+//
+//     cssScrollSnapPolyfill();
+// };
+// init();
+
+ const gra = function(min, max) {
+    return Math.random() * (max - min) + min;
+}
+const init = function(){
+    let items = document.querySelectorAll('section');
+    for (let i = 0; i < items.length; i++){
+        items[i].style.background = randomColor({luminosity: 'light'});
+    }
+    cssScrollSnapPolyfill()
+}
+init();
